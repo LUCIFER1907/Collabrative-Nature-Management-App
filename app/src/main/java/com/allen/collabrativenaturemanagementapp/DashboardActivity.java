cb7fb47521e98a8a1dacedaa7aa028938e752829
LUCIFER1907 <49206251+LUCIFER1907@users.noreply.github.com>
2021-09-25 16:36:34 +0530

    Navigation Drawer ready - need to add on clicks and remove extra logout

--- app/src/main/java/com/allen/collabrativenaturemanagementapp/DashboardActivity.java
+++ app/src/main/java/com/allen/collabrativenaturemanagementapp/DashboardActivity.java
@@ -1,21 +1,40 @@
 package com.allen.collabrativenaturemanagementapp;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
     private Button logout;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         logout=findViewById(R.id.logout);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
