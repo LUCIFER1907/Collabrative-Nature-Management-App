79378df3106134525e74133cadefe4c27303b070
LUCIFER1907 <49206251+LUCIFER1907@users.noreply.github.com>
2021-09-28 15:43:44 +0530

    Navigation Drawer

--- app/src/main/java/com/allen/collabrativenaturemanagementapp/DashboardActivity.java
+++ app/src/main/java/com/allen/collabrativenaturemanagementapp/DashboardActivity.java
@@ -1,21 +1,24 @@
 package com.allen.collabrativenaturemanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class DashboardActivity extends AppCompatActivity {
-    private Button logout;
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
@@ -24,7 +27,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        logout=findViewById(R.id.logout);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,14 +37,39 @@ public class DashboardActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id){
+            case R.id.home_icon :
+                Toast.makeText(DashboardActivity.this, "Home Icon Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.add_icon :
+                Toast.makeText(DashboardActivity.this, "add_icon Clicked", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(getApplicationContext(), diseases.class));
+                break;
+            case R.id.area :
+                Toast.makeText(DashboardActivity.this, "area Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout :
+                Toast.makeText(DashboardActivity.this, "logout Clicked", Toast.LENGTH_SHORT).show();
                 FirebaseAuth.getInstance().signOut();
                 Intent intent=new Intent(DashboardActivity.this,MainActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + id);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
