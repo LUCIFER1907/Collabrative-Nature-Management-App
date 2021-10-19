8000ef4b98a22cc6261031cf1380f3ed001caa21
LUCIFER1907 <49206251+LUCIFER1907@users.noreply.github.com>
2021-10-19 12:41:15 +0530

    Image upload to firebase storage, need to categorize it into database and add location

---
+++ app/src/main/java/com/allen/collabrativenaturemanagementapp/Model.java
@@ -0,0 +1,19 @@
+package com.allen.collabrativenaturemanagementapp;
+
+public class Model {
+    String ImageUrl;
+    public Model(){
+
+    }
+    public Model(String ImageUrl){
+        this.ImageUrl = ImageUrl;
+    }
+
+    public String getImageUrl() {
+        return ImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        ImageUrl = imageUrl;
+    }
+}
